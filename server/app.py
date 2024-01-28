367fd9629f7fffea7a7633b422de69e9815a7811
Reginawambui <ngendoanita8@gmail.com>
2024-01-28 22:50:00 +0300

    tables

--- server/app.py
+++ server/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db,Hero,Power,HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-if __name__ == '__main__':
-    app.run(port=5555)
+Migrate = Migrate(app,db)
+
+db.init_app(app)
+
+# @app.route('/heroes')
+# def heroes():
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 class Hero(db.Model):
@@ -6,15 +7,33 @@ class Hero(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    age = db.Column(db.integer)
+    age = db.Column(db.Integer)
 
 
+
+    Power_id = db.Column(db.Integer(), db.ForeignKey('powers.id')) 
 
 
 class Power(db.Model):
     __tablename__='powers'
 
 
-    id = db.Column(db.integer,primary_key = True)
-    number_of_powers = db.column(db.string)
+    id = db.Column(db.Integer,primary_key = True)
+    number_of_powers = db.column(db.String)
 
+    @validates("description")
+    def validate_description(self,key,description):
+        if "length" not in description :
+            raise ValueError("failed power validation")
+
+
+class HeroPower(db.Model):
+    __tablename__='heropowers'
+    
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    @validates('strength')
+    def validate_strength(self,key,strength):
+        if "presence" not in strength:
+            raise ValueError("failed heropower validation")
