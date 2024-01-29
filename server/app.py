9f9675393bb609063c0e83c171f5557d313374f5
Reginawambui <ngendoanita8@gmail.com>
2024-01-29 12:22:32 +0300

    routes

--- server/app.py
+++ server/app.py
@@ -13,25 +13,62 @@ Migrate = Migrate(app,db)
 
 db.init_app(app)
 
-# @app.route('/heroes')
-# def heroes():
-#     heroes = []
+@app.route('/heroes')
+def heroes():
+    heroes = []
 
-#     for hero in hero.query.all():
-#         hero_dict ={
-#             "name" : hero.name,
-#             "age"  : hero.age
+    for hero in Hero.query.all():
+        hero_dict ={
+            "name" : hero.name,
+            "age"  : hero.age
         
-#         }
-#         heroes.append(hero_dict)
+        }
+        heroes.append(hero_dict)
 
-#         response = make_response(
-#         jsonify(heroes),
-#             200
-#         )
-#         return response  
+        response = make_response(
+        jsonify(heroes),
+            200
+        )
+        return response  
 
-#     if __name__ == '__main__':
-#         app.run(port=5555)
+    if __name__ == '__main__':
+        app.run(port=5555)
                           
-     
+@app.route('/heroes/<int:id>')
+def hero_by_id(id):
+    hero = Hero.query.filter_by(id=id).first()
+
+    hero_dict = {
+        "name": hero.name,
+        "age": hero.age,
+       
+    }
+
+    response = make_response(
+        jsonify(hero_dict),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
+@app.route('/power')
+def power():
+    
+    powers =[]
+    for power in Power.query.all():
+        power_dict={
+
+            "number_of_power":power.number_of_power,
+            "description":power.description,
+            
+        }
+
+        power.append(power_dict)
+        response = make_response(
+        jsonify(powers),
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+
+    return response
--- server/seed.py
+++ server/seed.py
@@ -3,11 +3,12 @@ from random import choice as rc
 from faker import Faker
 
 from app import app
-from models import db, Hero, Power
+from models import db, Hero, Power,HeroPower
 
 db.init_app(app)
 
 fake = Faker()
+
 with app.app_context():
 
     Hero.query.delete()
@@ -21,6 +22,22 @@ with app.app_context():
     db.session.add_all(heroes)  
 
     powers = []
+    descriptions =['weak ','strong' , 'average']
+    for n in range (15):
+        power = power(number_of_powers=fake.number_of_powers(), description=rc(descriptions) )
+        powers.append(power)
+
+    db.session.add_all(powers)  
+
+
+    heropowers=[]
+    for n in range (10):
+        heropower = HeroPower(strength =fake.strength())
+        heropowers.append(hero)
+
+    db.session.add_all(heropowers)  
+
+
 
 
 
